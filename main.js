d1fedf1b901a2177d4290c7609dc241efb67ad75
Kuba Wasiak <qbawasiak@wp.pl>
2015-10-22 16:14:54 +0200

    dynamic creating rows and pictures

--- main.js
+++ main.js
@@ -25,6 +25,50 @@ var pictures = [
   'http://lorempixel.com/800/300/?4'
 ];
 
+var createPicturesRows = function() {
+  var mainContent = document.getElementById('main-content');
+  var numberOfPics = pictures.length - 1;
+  var currentPic = 1;
+  // how many rows to create if 0 pictures => pictures.length = 1 => floor(0.5) = 0
+  // if 1 picture => pictures.length =2 => floor(1) = 1
+  var numberOfRows = Math.floor(pictures.length / 2);
+  for (i = 1; i <= numberOfRows; i++) {
+    var row = document.createElement('div');
+    mainContent.appendChild(row);
+    row.classList.add('row');
+    var pic = document.createElement('div');
+    row.appendChild(pic);
+    //odd row (1,3,5)
+    if (i % 2 !== 0) {
+      pic.classList.add('col-xs-8');
+    } else {
+      pic.classList.add('col-xs-4');
+    }
+    pic.classList.add('pictures');
+    pic.id = 'pic-' + currentPic;
+    var image = document.createElement('img');
+    pic.appendChild(image);
+    image.src = pictures[currentPic];
+
+    if (pictures[currentPic + 1]) {
+      var pic = document.createElement('div');
+      row.appendChild(pic);
+      if (i % 2 !== 0) {
+        pic.classList.add('col-xs-4');
+      } else {
+        pic.classList.add('col-xs-8');
+      }
+      pic.classList.add('pictures')      
+      pic.id = 'pic-' + (currentPic + 1);
+      var image = document.createElement('img');
+      pic.appendChild(image);
+      image.src = pictures[currentPic + 1]; 
+    }
+    currentPic += 2;
+  } 
+}
+createPicturesRows();
+
 var setImages = function() {
   for (i = 1; i < pictures.length; i++) {
     var picDiv = document.getElementById('pic-' + i);
