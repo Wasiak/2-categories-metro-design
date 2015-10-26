0f7d451b8c30c1d4f1b5bf10d0bda123da522b94
Kuba Wasiak <qbawasiak@wp.pl>
2015-10-26 16:10:47 +0100

    pictures change on right category

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var categoryPics = [
   'index0',
   'http://lorempixel.com/g/500/250/',
-  'http://lorempixel.com/g/500/250/?2'
+  'http://lorempixel.com/500/250/?2'
 ];
 
 var setCategoryImages = function() {
@@ -12,22 +12,21 @@ var setCategoryImages = function() {
   }
 }
 setCategoryImages();
-// cat 1 big - cat 2 small
-// cat 1 small - cat 2 big
-// cat 1 big - cat 2 small
-//
-// cat [1,2,1,2,1,2]
-var pictures = [
-  'index0',
+
+// main page left pics of cat 1
+var mainPicCatOne = [
   'http://lorempixel.com/g/800/300/?3',
-  'http://lorempixel.com/400/300/?4',
   'http://lorempixel.com/g/400/300/?3',
-  'http://lorempixel.com/800/300/?4',
   'http://lorempixel.com/g/800/300/?3',
-  'http://lorempixel.com/400/300/?4',
-  'http://lorempixel.com/g/400/300/?3',
-  'http://lorempixel.com/800/300/?4'
+  'http://lorempixel.com/g/400/300/?3'
 ];
+//main page right pics of cat 2
+var mainPicCatTwo = [
+  'http://lorempixel.com/400/300/?4',
+  'http://lorempixel.com/800/300/?4',
+  'http://lorempixel.com/400/300/?4',
+  'http://lorempixel.com/800/300/?4'
+]
 //right pictures of category 1
 var cat1Pictures = [
   'http://lorempixel.com/g/400/300/?3',
@@ -45,11 +44,8 @@ var cat2Pictures = [
 
 var createPicturesRows = function() {
   var mainContent = $('#main-content');
-  var numberOfPics = pictures.length - 1;
   var currentPic = 1;
-  // how many rows to create if 0 pictures => pictures.length = 1 => floor(0.5) = 0
-  // if 1 picture => pictures.length =2 => floor(1) = 1
-  var numberOfRows = Math.floor(pictures.length / 2);
+  var numberOfRows = mainPicCatOne.length -1;
   for (i = 1; i <= numberOfRows; i++) {
     var row = document.createElement('div');
     mainContent.append(row);
@@ -62,26 +58,25 @@ var createPicturesRows = function() {
     } else {
       pic.classList.add('col-xs-4');
     }
-    pic.classList.add('pictures', 'left');
+    pic.classList.add('pictures', 'left', 'cat-1');
     pic.id = 'pic-' + currentPic;
     var image = document.createElement('img');
     pic.appendChild(image);
-    image.src = pictures[currentPic];
+    image.src = mainPicCatOne[i-1];
 
-    if (pictures[currentPic + 1]) {
-      var pic = document.createElement('div');
-      row.appendChild(pic);
-      if (i % 2 !== 0) {
-        pic.classList.add('col-xs-4');
-      } else {
-        pic.classList.add('col-xs-8');
-      }
-      pic.classList.add('pictures', 'right');      
-      pic.id = 'pic-' + (currentPic + 1);
-      var image = document.createElement('img');
-      pic.appendChild(image);
-      image.src = pictures[currentPic + 1]; 
+    var pic = document.createElement('div');
+    row.appendChild(pic);
+    if (i % 2 !== 0) {
+      pic.classList.add('col-xs-4');
+    } else {
+      pic.classList.add('col-xs-8');
     }
+    pic.classList.add('pictures', 'right', 'cat-2');      
+    pic.id = 'pic-' + (currentPic + 1);
+    var image = document.createElement('img');
+    pic.appendChild(image);
+    image.src = mainPicCatTwo[i-1]; 
+
     currentPic += 2;
   } 
 }
@@ -90,88 +85,85 @@ createPicturesRows();
 var displayCat1 = function() {
   $.each($('.right'), function(index, element) {
     $(element).find('img').attr('src', cat1Pictures[index]);
+    $(element).removeClass('cat-2');
+    $(element).addClass('cat-1');
   });
 }
 var displayCat2 = function() {
   $.each($('.left'), function(index, element) {
     $(element).find('img').attr('src', cat2Pictures[index]);
+    $(element).removeClass('cat-1');
+    $(element).addClass('cat-2');
+  });
+}
+var displayCat3 = function() {
+  $.each($('.left'), function(index, element) {
+    $(element).find('img').attr('src', mainPicCatOne[index]);
+    $(element).removeClass('cat-2');
+    $(element).addClass('cat-1');
+  });
+}
+var displayCat4 = function() {
+  $.each($('.right'), function(index, element) {
+    $(element).find('img').attr('src', mainPicCatTwo[index]);
+    $(element).removeClass('cat-1');
+    $(element).addClass('cat-2');
   });
 }
 
-// var categoryToShow = 'main';
 var showRightPictures = function() {
   $('.right-hide').removeClass('right-hide');
 }
-var hideRightPictures = function() {
+var hideRightPictures = function(c) {
   $('.right').addClass('right-hide');
-  $('.right').on('transitionend', displayCat1);
+  $('.right').on('transitionend', function() {
+     window['displayCat' + c]();
+  });
   $('.right').find('img').on('load', showRightPictures);
 }
 
 var showLeftPictures = function() {
   $('.left-hide').removeClass('left-hide');
 }
-var hideLeftPictures = function() {
+var hideLeftPictures = function(c) {
   $('.left').addClass('left-hide');
-  $('.left').on('transitionend', displayCat2);
+  $('.left').on('transitionend', function() {
+    window['displayCat' + c]();
+  });
   $('.left').find('img').on('load', showLeftPictures);
 }
 
-
-var setMainImages = function() {
-  for (i = 1; i < pictures.length; i++) {
-    var picDiv = document.getElementById('pic-' + i);
-    var pic = picDiv.getElementsByTagName('img')[0];
-    pic.src = pictures[i]; 
-  }   
-}
-
-setMainImages();
-// var setImages = function() {
-//   console.log(categoryToShow);
-//   if  (categoryToShow === 'main') {
-//     for (i = 1; i < pictures.length; i++) {
-//       var picDiv = document.getElementById('pic-' + i);
-//       var pic = picDiv.getElementsByTagName('img')[0];
-      
-//       pic.src = pictures[i];
-//     }
-//   } else if (categoryToShow === 'first') {
-//     for (i = 2; i < pictures.length; i += 2) {
-//       var picDiv = document.getElementById('pic-' + i);
-//       var pic = picDiv.getElementsByTagName('img')[0];
-//       hideRightPictures(picDiv);
-      
-//       picDiv.addEventListener('transitionend', function(){
-//         // showRightPictures(picDiv);
-//         console.log('transition finish');
-//         var hiddenPics = document.getElementsByClassName('right-hide');
-//         for (i = 0; i < hiddenPics.length; i++) {
-//           hiddenPics[i].classList.remove('right-hide');
-//         }
-//       });
-//       pic.src = cat1Pictures[i/2];
-//       // picDiv.addEventListener('transitionend', showRightPictures(picDiv));
-      
-//     }
-//   } else if (categoryToShow === 'second') {
-//     for (i = 1; i < pictures.length; i += 2) {
-//       var picDiv = document.getElementById('pic-' + i);
-//       var pic = picDiv.getElementsByTagName('img')[0];
-//       hideLeftPictures(picDiv);
-      
-//       picDiv.addEventListener('transitionend', function(){
-//         console.log('transition finish');
-//         var hiddenPics = document.getElementsByClassName('left-hide');
-//         for (i = 0; i < hiddenPics.length; i++) {
-//           hiddenPics[i].classList.remove('left-hide');
-//         }
-//       });
-//       pic.src = cat2Pictures[Math.ceil(i/2)];
-//     }
-//   }
+// var setMainImages = function() {
+//   for (i = 1; i < mainPicCatOne.length; i += 2) {
+//     var picDiv = document.getElementById('pic-' + i);
+//     var pic = picDiv.getElementsByTagName('img')[0];
+//     pic.src = mainPicCatOne[i];
+//     var picDiv2 = document.getElementById('pic-' + i + 1);
+//     var pic2 = picDiv2.getElementsByTagName('img')[0]; 
+//     pic2.src = mainPicCatTwo[i];
+//   }   
 // }
-// setImages(); 
+
+// setMainImages();
+
+var showCategoryOne = function() {
+  if ($('.left').hasClass('cat-1') && $('.right').hasClass('cat-2')) {
+    hideRightPictures(1);
+  } else if ($('.right').hasClass('cat-1')) {
+  } else {
+    hideRightPictures(1);
+    hideLeftPictures(3);
+  }
+}
+var showCategoryTwo = function() {
+  if ($('.right').hasClass('cat-2') && $('.left').hasClass('cat-1')) {
+    hideLeftPictures(2);
+  } else if ($('.left').hasClass('cat-2')){
+  } else {
+    hideLeftPictures(2);
+    hideRightPictures(4);
+  }
+}
  
-$('#cat-1').on('click', hideLeftPictures);
-$('#cat-2').on('click', hideRightPictures);
+$('#cat-1').on('click', showCategoryOne);
+$('#cat-2').on('click', showCategoryTwo);
